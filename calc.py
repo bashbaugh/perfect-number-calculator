1880d4541b0d0783ca49856f2e689d92ee321eef
Benjamin <dogle22@outlook.com>
2017-10-09 14:23:22 -0600

    first working draft

--- calc.py
+++ calc.py
@@ -1,1 +1,23 @@
 #Benjamin's perfect number calculator
+
+currnum = 1
+
+with open("results.txt", "a") as f:
+    f.write("perfect numbers:")
+
+
+def calculate():
+    global currnum
+    divisors = []
+    for x in range (1, currnum - 1):
+        if currnum % x == 0:
+            divisors.append(x)
+    divs_added = sum(divisors)
+    if currnum == divs_added:
+        print ("perfect number found: " + str(currnum))
+        with open("results.txt", "a") as f:
+            f.write("\n " + str(currnum))
+    currnum += 1
+
+while True:
+    calculate()
